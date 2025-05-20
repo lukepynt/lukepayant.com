c90db05f24fce7fb416405e24c5a7dea05c02857
lukepynt <lukepayant@gmail.com>
2025-05-20 12:31:59 -0400

    style changes to home page. still need to center the skule button

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(`hello`);
+// const btn1 = document.querySelectorAll(".btn1");
+
+// btn1.forEach(element => {
+//     element.onclick = () =>{
+//         const mask = 'radial-gradient(circle at 0px 0px, transparent 50px, black 60px)';
+//         element.parentElement.parentElement.style.maskImage = mask;
+//     }
+// });
