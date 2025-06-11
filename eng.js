8aeed5637c71045af00650d8409cd0bb335f8307
lukepynt <lukepayant@gmail.com>
2025-06-10 20:18:52 -0400

    fixed search engine, now working on adding projects

--- eng.js
+++ eng.js
@@ -1,29 +1,25 @@
-const searchInput = document.getElementById("searchBar");
-const boxes = document.getElementById("projects").children;
+const projectBoxes = document.getElementById("projects").children;
 
-let projects = Array.from(boxes).map(box =>{
+let projects = Array.from(projectBoxes).map(box =>{
     const name = box.querySelector("h1").textContent;
     const desc = box.querySelector("p").textContent;
     const keywords = box.dataset.keywords.split(" ");
     return{box: box, name: name.toLowerCase(), desc: desc.toLowerCase(), keywords: keywords}
 });
-// Gets the search input everytime the search is editted
-searchInput.addEventListener("input", event => {
-    const search = event.target.value.toLowerCase();
-    projects.forEach(project => {
-        const visible = project.name.includes(search) || project.desc.includes(search) || project.keywords.some(keyword => keyword.includes(search));
-        project.box.classList.toggle("hide", !visible);
-    })
-})
-// For the filter selection
-const filters = document.getElementById("searchFilters");
 
-filters.addEventListener("change", () => {
-    const active = Array.from(filters.querySelectorAll("input[type='checkbox']:checked")).map(filter => filter.value);
+// Change visibility of projects according to inputs
+const searchInput = document.getElementById("searchBar");
+searchInput.addEventListener("input", inputChange);
+
+const filters = document.getElementById("searchFilters");
+filters.addEventListener("change", inputChange);
+
+function inputChange() {
+    const activeFilters = Array.from(filters.querySelectorAll("input[type='checkbox']:checked")).map(filter => filter.value);
+    const search = searchInput.value.toLowerCase();
     projects.forEach(project => {
-        const visible = active.length == 0 || project.keywords.some(keyword => {
-            return active.some(filter => keyword == filter);
-        });
-        project.box.classList.toggle("hide", !visible);
+        const passedFilters = project.keywords.some(keyword => activeFilters.every(filter => keyword == filter)) || activeFilters.length == 0;
+        const passedSearch = project.name.includes(search) || project.desc.includes(search) || project.keywords.some(keyword => keyword.includes(search));
+        project.box.classList.toggle("hide", !passedFilters || !passedSearch);
     })
-})
+}
