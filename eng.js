f070338d2a270691008618958bf18232171934ab
lukepynt <lukepayant@gmail.com>
2025-07-12 20:09:54 -0400

    animated opening of project and first attempt for responsive project page

--- eng.js
+++ eng.js
@@ -28,7 +28,40 @@ function inputChange() {
 // ----Opening Projects
 const openBtns = document.querySelectorAll(".btn3");
 openBtns.forEach(btn => {
+    // Open function
     btn.addEventListener("click", () => {
-        btn.parentElement.nextElementSibling.classList.toggle("hide");
+        const project = btn.parentElement.parentElement;
+
+        // Create clone to get the final measurements of the project for animating
+        const clone = project.cloneNode(true);
+        project.parentElement.appendChild(clone);
+        clone.style.visibility = "hidden";
+        clone.style.position = "absolute";
+
+        clone.classList.toggle("closedProject");
+        
+        project.style.width = project.offsetWidth + "px";
+        project.style.height = project.offsetHeight + "px";
+        void project.offsetWidth; //force browser to register the current width and height before changing.
+
+        const closed = project.classList.contains("closedProject");
+        if(!closed){
+            project.style.transition = "width 0.75s cubic-bezier(0.72, 0, 0.26, 0.95) 0.25s, height 0.5s cubic-bezier(0.72, 0, 0.26, 0.95), border-radius 0.25s ease";
+            project.classList.toggle("closedProject");
+        }else{
+            project.style.transition = "width 0.75s cubic-bezier(0.72, 0, 0.26, 0.95), height 0.5s cubic-bezier(0.72, 0, 0.26, 0.95) 0.25s";
+        }
+        project.style.width = clone.offsetWidth + "px";
+        project.style.height = clone.offsetHeight + "px";
+        setTimeout(() => {
+            project.style.width = "";
+            project.style.height = "";
+            if(closed) project.classList.toggle("closedProject");
+            project.style.transition = "";
+        }, 750);
+
+        btn.classList.toggle("expandedBtn");
+
+        clone.remove();   // Delete the clone
     })
 })
