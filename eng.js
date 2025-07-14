56ac206a7182574766b4779bdd985b71f903df04
lukepynt <lukepayant@gmail.com>
2025-07-14 12:26:15 -0400

    project animation update. animation and timing is dynamic depending on target size

--- eng.js
+++ eng.js
@@ -30,6 +30,7 @@ const openBtns = document.querySelectorAll(".btn3");
 openBtns.forEach(btn => {
     // Open function
     btn.addEventListener("click", () => {
+        btn.disabled = true;
         const project = btn.parentElement.parentElement;
 
         // Create clone to get the final measurements of the project for animating
@@ -44,27 +45,45 @@ openBtns.forEach(btn => {
         project.style.height = project.offsetHeight + "px";
         void project.offsetWidth; //force browser to register the current width and height before changing.
 
+        //animation timings
+        let widthTime = [0.5, 0];
+        let heightTime = [0.25, 0];
+        let borderTime = [0.25, 0];
         const closed = project.classList.contains("closedProject");
+        if(project.offsetWidth === clone.offsetWidth){
+            widthTime[0] = 0;
+        }
+        if(project.offsetHeight === clone.offsetHeight){
+            heightTime[0] = 0;
+        }
         if(!closed){
-            //closing animation
-            project.style.transition = "width 0.5s cubic-bezier(0.49, 0.02, 0.52, 0.97) 0.25s, height 0.25s cubic-bezier(0.72, 0, 0.26, 0.95), border-radius 0.25s ease 0.25s";
+            //closing animation (width and border change when height finished)
+            widthTime[1] = heightTime[0];
+            borderTime[1] = heightTime[0];
             project.classList.toggle("closedProject");
         }else{
-            //opening animation
-            project.style.transition = "width 0.5s cubic-bezier(0.49, 0.02, 0.52, 0.97), height 0.25s cubic-bezier(0.72, 0, 0.26, 0.95) 0.5s, border-radius 0.25s ease 0.5s";
+            //opening animation (height and border change when width finished)
+            heightTime[1] = widthTime[0];
+            borderTime[1] = widthTime[0];
             project.style.borderRadius = getComputedStyle(clone).borderRadius;
         }
-        if(project.offsetWidth != clone.offsetWidth){
-            project.style.width = clone.offsetWidth + "px";
-        }
+        //animate width/height only if width/height changes
+        project.style.transition = `width ${widthTime[0]}s cubic-bezier(0.49, 0.02, 0.52, 0.97) ${widthTime[1]}s,
+                                    height ${heightTime[0]}s cubic-bezier(0.72, 0, 0.26, 0.95) ${heightTime[1]}s,
+                                    border-radius ${borderTime[0]}s ease ${borderTime[1]}s`
+                                    
+        project.style.width = clone.offsetWidth + "px";
         project.style.height = clone.offsetHeight + "px";
+
+        const totTime = Math.max(widthTime[0]+widthTime[1], heightTime[0]+heightTime[1], borderTime[0]+borderTime[1]) * 1000;
         setTimeout(() => {
             project.style.width = "";
             project.style.height = "";
             project.style.borderRadius = "";
             if(closed) project.classList.toggle("closedProject");
             project.style.transition = "";
-        }, 750);
+            btn.disabled = false;
+        }, totTime);
 
         btn.classList.toggle("expandedBtn");
 
