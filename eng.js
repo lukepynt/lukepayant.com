6156817e25b36ce5a6e9488c015321a5f8bd3689
lukepynt <lukepayant@gmail.com>
2025-07-13 21:02:03 -0400

    test for buggy animation fix. project width only animates if it changes

--- eng.js
+++ eng.js
@@ -47,13 +47,14 @@ openBtns.forEach(btn => {
         const closed = project.classList.contains("closedProject");
         if(!closed){
             //closing animation
-            project.style.transition = "width 0.5s cubic-bezier(0.49, 0.02, 0.52, 0.97) 0.25s, height 0.5s cubic-bezier(0.72, 0, 0.26, 0.95), border-radius 0.25s ease";
+            project.style.transition = "width 0.5s cubic-bezier(0.49, 0.02, 0.52, 0.97) 0.25s, height 0.25s cubic-bezier(0.72, 0, 0.26, 0.95), border-radius 0.25s ease 0.25s";
             project.classList.toggle("closedProject");
+            console.log(project.offsetWidth);
         }else{
             //opening animation
-            project.style.transition = "width 0.75s cubic-bezier(0.49, 0.02, 0.52, 0.97), height 0.5s cubic-bezier(0.72, 0, 0.26, 0.95) 0.25s";
+            project.style.transition = "width 0.5s cubic-bezier(0.49, 0.02, 0.52, 0.97), height 0.5s cubic-bezier(0.72, 0, 0.26, 0.95) 0.25s, border-radius 0.25s ease";
         }
-        project.style.width = clone.offsetWidth + "px";
+        project.style.width = project.style.width === clone.offsetWidth? clone.offsetWidth + "px": "";
         project.style.height = clone.offsetHeight + "px";
         setTimeout(() => {
             project.style.width = "";
